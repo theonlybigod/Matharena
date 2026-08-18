c72ce53584131ce3a5cf40e24f038df1e7ab364b
vedankk <vedankoushik@gmail.com>
2026-08-18 12:47:30 -0400

    Remove temporary test scripts

--- src/ServerScriptService/TestMatchSystem.server.lua
+++
@@ -1,7 +0,0 @@
---[[
-	This temporary test file has been neutralized (Claude has no delete
-	capability for the local filesystem). It served its purpose verifying
-	MatchSystem in Message 5 and can be safely deleted:
-
-		src/ServerScriptService/TestMatchSystem.server.lua
-]]
--- src/ServerScriptService/TestProgressionSystem.server.lua
+++
@@ -1,7 +0,0 @@
---[[
-	This temporary test file has been neutralized (Claude has no delete
-	capability for the local filesystem). It served its purpose verifying
-	ProgressionSystem in Message 4 and can be safely deleted:
-
-		src/ServerScriptService/TestProgressionSystem.server.lua
-]]
